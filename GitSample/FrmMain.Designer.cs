d9400406f53d79a3b5a06713cca51222922790df
keimatsushima <k.matsushima122@gmail.com>
2024-02-24 17:28:23 +0900

    割り算追加

--- GitSample/FrmMain.Designer.cs
+++ GitSample/FrmMain.Designer.cs
@@ -36,6 +36,7 @@
             this.txtResult = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             label3 = new System.Windows.Forms.Label();
@@ -123,6 +124,18 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.button3.Location = new System.Drawing.Point(192, 103);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "÷";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
@@ -154,6 +167,7 @@
         private System.Windows.Forms.TextBox txtResult;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
